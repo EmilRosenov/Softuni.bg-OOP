e4b546801c231a188e35e3f827e3b21f5d84d30e
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2022-01-20 14:22:04 +0000

    Add files via upload

---
+++ 01.InheritanceLab/01.SingleInheritance/StartUp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Farm
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Dog brian = new Dog();
+            brian.Bark();
+            brian.Bark();
+            brian.Eat();
+        }
+    }
+}
