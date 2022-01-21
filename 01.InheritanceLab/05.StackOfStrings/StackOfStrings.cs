2260d966b4a9c6204398f4e402d42b8e9393f4c0
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2022-01-21 14:11:05 +0000

    Add files via upload

---
+++ 01.InheritanceLab/05.StackOfStrings/StackOfStrings.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomStack
+{
+    public class StackOfStrings:Stack<string>
+    {
+        Stack<string> list;
+        public int count;
+
+        public StackOfStrings()
+        {
+            list = new Stack<string>();
+            
+    }
+        public int Count => this.count;
+        public bool IsEmpty()
+        {
+            if (list.Count==0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public void AddRange(params string[] data)
+        {
+            foreach (var item in data)
+            {
+                list.Push(item);
+                count++;
+            }
+        }
+    }
+}
---
+++ 01.InheritanceLab/05.StackOfStrings/StartUp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CustomStack
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            StackOfStrings stack = new StackOfStrings();
+            Console.WriteLine(stack.IsEmpty());
+            stack.AddRange("hello", "world", "how", "are", "you");
+            Console.WriteLine(stack.IsEmpty());
+            Console.WriteLine(stack.Count);
+        }
+    }
+}
