5d748dc50357a43713e87ca0942807d2c936359a
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2022-01-21 14:10:35 +0000

    Add files via upload

---
+++ 01.InheritanceLab/04.RandomList/RandomList.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomRandomList
+{
+    public class RandomList : List<string>
+    {
+        private Random rand;
+        
+        public RandomList()
+        {
+            rand = new Random();   
+        }
+       
+        public string GetRandomElement()
+        {
+            var index = rand.Next(0, Count);
+            return this[index];
+        }
+        public string RemoveRandomElement()
+        {
+            var index = rand.Next(0, Count);
+            string elementToRemove = this[index];
+            this.RemoveAt(index);
+            return elementToRemove;
+        }
+    }
+}
---
+++ 01.InheritanceLab/04.RandomList/StartUp.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace CustomRandomList
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            RandomList listRandom = new RandomList();
+            string input = Console.ReadLine();
+            string input2 = Console.ReadLine();
+            string input3 = Console.ReadLine();
+            string input4 = Console.ReadLine();
+            string input5 = Console.ReadLine();
+
+            listRandom.Add(input);
+            listRandom.Add(input2);
+            listRandom.Add(input3);
+            listRandom.Add(input4);
+            listRandom.Add(input5);
+           
+            //listRandom.RemoveRandomElement();
+            //listRandom.RemoveRandomElement();
+            Console.WriteLine(listRandom.RemoveRandomElement());
+            Console.WriteLine(listRandom.RemoveRandomElement());
+            Console.WriteLine(listRandom.GetRandomElement()); 
+
+            Console.WriteLine(string.Join(", ",listRandom));
+            Console.WriteLine(listRandom.Count);
+           
+        }
+    }
+}
