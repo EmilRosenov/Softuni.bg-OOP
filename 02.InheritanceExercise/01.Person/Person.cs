be507468573ed6f616684ddc2e3c255e965621f5
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2022-01-22 13:19:35 +0000

    Add files via upload

---
+++ 02.InheritanceExercise/01.Person/Person.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Person
+{
+    public class Person
+{
+        public string name;
+        public int age;
+
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            Age = age;
+        }
+
+        public string Name { get; set; }
+        public int Age 
+        {
+            get { return this.age; }
+            
+            set
+            {
+                if (this.age < 0)
+                {
+                    throw new Exception();
+                }
+                else
+                {
+                    age = value;
+                    //return value;
+                        
+                }
+            }
+        }
+
+
+        public override string ToString()
+        {
+            return $"Name: {this.Name}, Age: {this.Age}";
+          
+        }
+    }
+}
