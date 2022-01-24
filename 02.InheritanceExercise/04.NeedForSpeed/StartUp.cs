3393140627b2d4f921081c11b9f4629decb8337b
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2022-01-24 22:41:43 +0000

    Add files via upload

---
+++ 02.InheritanceExercise/04.NeedForSpeed/StartUp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace NeedForSpeed
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            int horsePower = int.Parse(Console.ReadLine());
+            double fuel = double.Parse(Console.ReadLine());
+
+            SportCar sportCar = new SportCar(horsePower,fuel);
+
+            sportCar.Drive(10);
+            
+            Console.WriteLine(sportCar.Fuel);
+            FamilyCar car = new FamilyCar(horsePower, fuel);
+            car.Drive(10);
+            Console.WriteLine(car.Fuel);
+            RaceMotorcycle raceMoto = new RaceMotorcycle(horsePower, fuel);
+            raceMoto.Drive(10);
+            Console.WriteLine(raceMoto.Fuel);
+
+        }
+    }
+}
