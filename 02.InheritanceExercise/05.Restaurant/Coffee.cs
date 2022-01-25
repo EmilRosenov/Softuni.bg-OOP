06449bca5ee7b82410a14605e4cdc384afa85190
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2022-01-25 16:12:32 +0000

    Add files via upload

---
+++ 02.InheritanceExercise/05.Restaurant/Coffee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant
+{
+    public class Coffee : HotBeverage
+    {
+        private const decimal CoffeePrice = 3.50M;
+        private const double  CoffeeMililiters = 3.50;
+        public Coffee(string name,double caffeine) 
+            : base(name, CoffeePrice, CoffeeMililiters)
+        {
+            this.Caffeine = caffeine;
+        }
+        public double Caffeine { get; set; }
+    }
+}
---
+++ 02.InheritanceExercise/05.Restaurant/StartUp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Restaurant
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            decimal price = decimal.Parse(Console.ReadLine());
+            double grams = double.Parse(Console.ReadLine());
+            double calories = double.Parse(Console.ReadLine());
+            
+            Cake orangeCake = new Cake(name);
+
+            Console.WriteLine(orangeCake.Calories);
+            Console.WriteLine(orangeCake.Grams);
+            Console.WriteLine(orangeCake.Price);
+
+            Fish fish = new Fish(name, price);
+            Console.WriteLine(fish.Grams);
+            
+
+        }
+    }
+}
