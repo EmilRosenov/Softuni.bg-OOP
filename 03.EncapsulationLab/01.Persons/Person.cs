e6f02bc248c125d4dee6725d849aa894aed3513b
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2022-01-27 16:00:12 +0000

    Add files via upload

---
+++ 03.EncapsulationLab/01.Persons/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonsInfo
+{
+    public class Person
+    {
+        List<Person> personList;
+        public Person(string firstName, string lastName, int age)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+            personList = new List<Person>();
+        }
+
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public int Age { get; private set; }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName} is {Age} years old.";
+        }
+    }
+}
---
+++ 03.EncapsulationLab/01.Persons/StartUp.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PersonsInfo
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var lines = int.Parse(Console.ReadLine());
+            var persons = new List<Person>();
+
+            for (int i = 0; i < lines; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+                string name = input[0];
+                string secondName = input[1];
+                int age = int.Parse(input[2]);
+
+                Person person = new Person(name, secondName, age);
+                persons.Add(person);
+            }
+
+            persons = persons.OrderBy(x => x.FirstName).ThenBy(x => x.Age).ToList();
+
+            foreach (var item in persons)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
