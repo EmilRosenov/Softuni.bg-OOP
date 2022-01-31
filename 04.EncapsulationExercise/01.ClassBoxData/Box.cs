086c1bfccbe3392fc946b196c5e0873da8af60c6
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2022-01-31 03:09:41 +0000

    Add files via upload

---
+++ 04.EncapsulationExercise/01.ClassBoxData/Box.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _01.ClassBoxData
+{
+    public class Box
+    {
+        private double length;
+        private double width;
+        private double height;
+
+        public Box(double length, double width, double height)
+        {
+            Length = length;
+            Width = width;
+            Height = height;
+        }
+
+        public double Length
+        { get=>this.length;
+            set
+            {
+                ThrowIfInvalidSide(value, nameof(Length));
+                this.length = value;
+            } 
+        
+        }
+        public double Width
+        {
+            get => this.width;
+            set
+            {
+                ThrowIfInvalidSide(value, nameof(Width));
+                this.width = value;
+            }
+
+        }
+        public double Height
+        {
+            get => this.height;
+            set
+            {
+                ThrowIfInvalidSide(value, nameof(Height));
+                this.height = value;
+            }
+
+        }
+        public double SurfaceArea()
+        {
+            return 2*(Length*Width) + 2*(Length*Height) +  2*(Width*Height);
+        }
+        public double LateralSurfaceArea()
+        {
+            return  2 * Length * Height + 2 * Width * Height;
+        }
+        public double Volume()
+        {
+            return Length*Width*Height;
+        }
+
+        private void ThrowIfInvalidSide(double value, string side)
+        {
+            if (value <= 0)
+            {
+                throw new ArgumentException($"{side} cannot be zero or negative.");
+            }
+        }
+    }
+}
---
+++ 04.EncapsulationExercise/01.ClassBoxData/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _01.ClassBoxData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double lenght = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+
+            try
+            {
+                var box = new Box(lenght, width, height);
+
+                Console.WriteLine($"Surface Area - {box.SurfaceArea():f2}");
+                Console.WriteLine($"Lateral Surface Area - {box.LateralSurfaceArea():f2}");
+                Console.WriteLine($"Volume - {box.Volume():f2}");
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+           
+        }
+    }
+}
