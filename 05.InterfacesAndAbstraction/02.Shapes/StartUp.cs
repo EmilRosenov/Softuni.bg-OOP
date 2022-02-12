97bbd3e3ae0eba90efa5ff0e0cf4fccae863efb6
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2022-02-12 19:14:24 +0000

    Add files via upload

---
+++ 05.InterfacesAndAbstraction/02.Shapes/StartUp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Shapes
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<IDrawable> drawableObjects = new List<IDrawable>();
+            IDrawable rectangle = new Rectangle(6, 4);
+            //rectangle.Draw();
+            IDrawable circle = new Circle(3);
+            //circle.Draw();
+
+            drawableObjects.Add(circle);
+            drawableObjects.Add(rectangle);
+            
+            foreach (IDrawable item in drawableObjects)
+            {
+                item.Draw();
+            }
+        }
+    }
+}
