7cd3abf46cb84a3fde10f104ba7deea30e65e9e6
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2022-02-13 20:54:27 +0000

    Add files via upload

---
+++ 06.InterfacesAndAbstractionExercise/02.MultipleImplementation/StartUp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace PersonInfo
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+            string id = Console.ReadLine();
+            string birthdate = Console.ReadLine();
+
+            IIdentifiable person = new Citizen(name, age, id, birthdate);
+            Console.WriteLine(person.Id);
+            IBirthable otherPerson = new Citizen(name, age, id, birthdate);
+            Console.WriteLine(otherPerson.Birthdate);
+
+
+        }
+    }
+}
