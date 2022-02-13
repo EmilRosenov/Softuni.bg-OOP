1bc1977a5efad37df076fa3f58310819180df800
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2022-02-13 19:03:38 +0000

    Add files via upload

---
+++ 05.InterfacesAndAbstraction/04.Cars/Seat.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cars
+{
+    public class Seat : ICar
+    {
+        public Seat(string model, string color)
+        {
+            Model = model;
+            Color = color;
+        }
+
+        public string Model { get; }
+
+        public string Color { get; }
+
+        public string Start()
+        {
+            return "Engine start";
+        }
+
+        public string Stop()
+        {
+            return "Breaaak!";
+        }
+        public override string ToString()
+        {
+            return $"{Color} Seat {Model}"; 
+        }
+    }
+}
