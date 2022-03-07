0b9dac1ca96ceaa9f0a3451e5929fa3d7959f1a8
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2022-03-07 22:29:56 +0000

    Add files via upload

---
+++ 07.PolymorphismLab/05.Animals/Animal.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals
+{
+    public abstract class Animal
+    {
+        public Animal(string name, string favFood)
+        {
+            Name = name;
+            FavFood = favFood;
+        }
+
+        public string Name { get; set; }
+        public string FavFood { get; set; }
+
+        public abstract string ExplainSelf();
+
+        public override string ToString()
+        {
+            return $"I am {this.Name} and my fovourite food is {this.FavFood}";
+        }
+    }
+}
---
+++ 07.PolymorphismLab/05.Animals/Dog.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals
+{
+    public class Dog : Animal
+    {
+        public Dog(string name, string favFood) 
+            : base(name, favFood)
+        {
+        }
+
+        public override string ExplainSelf()
+        {
+            return $"{base.ToString()} \nDJAAF";
+        }
+    }
+}
