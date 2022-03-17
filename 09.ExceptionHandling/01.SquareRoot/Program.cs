554280b4512d081274f7d95e43a858a7f959c3c1
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2022-03-17 23:14:41 +0000

    Add files via upload

---
+++ 09.ExceptionHandling/01.SquareRoot/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _01.SquareRoot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double input = double.Parse(Console.ReadLine());
+
+
+            if (Math.Sqrt(input) >= 0)
+            {
+                try
+                {
+                    Console.WriteLine(Math.Sqrt(input));
+
+                }
+                catch
+                {
+                    Console.WriteLine("Invalid number.");
+                }
+                finally
+                {
+                    Console.WriteLine("Goodbye.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid number.");
+                Console.WriteLine("Goodbye.");
+            }
+
+        }
+    }
+}
