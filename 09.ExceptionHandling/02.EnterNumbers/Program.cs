1a3f89786f26085d6acd9a7aed0436a4a29ef116
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2022-03-18 22:42:20 +0000

    Add files via upload

---
+++ 09.ExceptionHandling/02.EnterNumbers/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace EnterNumbers
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class EnterNumbersClass
+    {
+        public static void Main()
+        {
+            int counter = 0;
+            int start = 1;
+            HashSet<int> numbers = new HashSet<int>();
+            while (counter < 10)
+            {
+                int currentNum = 0;
+                try
+                {
+                    currentNum = ReadNumbers(start, 100);
+
+                    if (currentNum > start)
+                    {
+                        start = currentNum;
+                    }
+                    numbers.Add(currentNum);
+                    counter++;
+                }
+                catch (ArgumentOutOfRangeException)
+                {
+                    Console.WriteLine($"Your number is not in range {start} - 100!");
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Invalid Number!");
+                }
+            }
+            Console.WriteLine(string.Join(", ", numbers));
+        }
+
+        public static int ReadNumbers(int start, int end)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            if (num <= start || num >= end)
+            {
+                throw new ArgumentOutOfRangeException($"Your number is not in range {num} - 100!");
+            }
+
+            return num;
+        }
+    }
+}
