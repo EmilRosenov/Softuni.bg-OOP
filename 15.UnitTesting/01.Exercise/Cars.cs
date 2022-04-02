968bce4f3b593500b6ea4c19a216cb0183fdda99
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2022-04-02 18:15:57 +0100

    Add files via upload

---
+++ 15.UnitTesting/01.Exercise/Cars.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _01.Exercise
+{
+    public class Cars
+    {
+        public int Mileage { get; set; }
+
+        public void Drive(int km)
+        {
+            if (km<=0)
+            {
+                throw new InvalidOperationException("Don't cheat!");
+            }
+
+            Mileage += km;
+        }
+    }
+}
